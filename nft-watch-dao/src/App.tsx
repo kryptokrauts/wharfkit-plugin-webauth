c2f6d5fcbbf421b4596b725194ab4a9b767f2860
Marco Walz <8124114+marc0olo@users.noreply.github.com>
2025-02-23 13:35:01 +0100

    Update App.tsx

--- nft-watch-dao/src/App.tsx
+++ nft-watch-dao/src/App.tsx
@@ -395,7 +395,7 @@ function App() {
         data: {
           from: sess.actor,
           to: "nftwatchdao",
-          quantity: "10000.0000 XPR",
+          quantity: "4000.0000 XPR",
           memo: "shielding",
         },
       })
@@ -1168,7 +1168,7 @@ function App() {
                             placeholder='e.g. already creator of another shielded collection'
                             onChange={handleSkipReasonToReqShield}
                           ></TETextarea>
-                          <p className="italic">Note: You will send <b>10,000.0000 XPR</b> (<Link className="underline" to="https://docs.nftwatchdao.com/shielding/#shielding-fee" target="_blank">Shielding Fee</Link>) to the nftwatchdao account when signing the transaction!</p>
+                          <p className="italic">Note: You will send <b>4,000.0000 XPR</b> (<Link className="underline" to="https://docs.nftwatchdao.com/shielding/#shielding-fee" target="_blank">Shielding Fee</Link>) to the nftwatchdao account when signing the transaction!</p>
                           <button type="button"
                                   className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] disabled:opacity-70"
                                   onClick={doRequestShield}
